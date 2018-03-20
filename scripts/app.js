154b082c95313e72b0a84b4eb6beb0333bca2db7
deadmaus <andrew.douglas758@gmail.com>
2018-03-20 20:19:15 +1100

    Add init, hide results div.

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,44 @@
+var questions = [];
+
+var question1 = {
+    "id": "1",
+    "type": "image",
+    "imageUrl": "/images/1.png",
+    "imageCaption": "caption"
+};
+
+var question2 = {
+    "id": "1",
+    "type": "image",
+    "imageUrl": "/images/1.png",
+    "imageCaption": "caption"
+};
+
+var question3 = {
+    "id": "1",
+    "type": "image",
+    "imageUrl": "/images/1.png",
+    "imageCaption": "caption"
+};
+
+var question4 = {
+    "id": "1",
+    "type": "image",
+    "imageUrl": "/images/1.png",
+    "imageCaption": "caption"
+};
+
+questions.push(question1);
+questions.push(question2);
+questions.push(question3);
+questions.push(question4);
+
 $(document).ready(function() {
     console.log("loaded");
+    init();
 });
+
+function init() {
+    // get divs
+    var divs = $("#results-section").hide();
+}
