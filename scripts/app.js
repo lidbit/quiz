29db4f9c288c0803b42597265c852b40a41b3af9
deadmaus <andrew.douglas758@gmail.com>
2018-03-21 23:23:41 +1100

    Question editor begin wip.

--- scripts/app.js
+++ scripts/app.js
@@ -10,21 +10,21 @@ var question1 = {
     "imageUrl": "images/image1.png",
     "imageCaption": "caption",
     "answers": [{
-        "choice": "7 colours",
-        "correct": false
-    },
-    {
-        "choice": "3 colours",
-        "correct": true
-    },
-    {
-        "choice": "2 colours",
-        "correct": false
-    },
-    {
-        "choice": "11 colours",
-        "correct": false
-    }
+            "choice": "7 colours",
+            "correct": false
+        },
+        {
+            "choice": "3 colours",
+            "correct": true
+        },
+        {
+            "choice": "2 colours",
+            "correct": false
+        },
+        {
+            "choice": "11 colours",
+            "correct": false
+        }
     ]
 };
 
@@ -33,13 +33,13 @@ var question2 = {
     "type": "type2",
     "questionText": "What are three primary numbers?",
     "answers": [{
-        "imageUrl": "images/image3.png",
-        "correct": false
-    },
-    {
-        "imageUrl": "images/image4.png",
-        "correct": true
-    },
+            "imageUrl": "images/image3.png",
+            "correct": false
+        },
+        {
+            "imageUrl": "images/image4.png",
+            "correct": true
+        },
     ]
 };
 
@@ -50,21 +50,21 @@ var question3 = {
     "imageUrl": "images/image3.png",
     "imageCaption": "caption",
     "answers": [{
-        "imageUrl": "images/image1.png",
-        "correct": false
-    },
-    {
-        "imageUrl": "images/image2.png",
-        "correct": true
-    },
-    {
-        "imageUrl": "images/image3.png",
-        "correct": false
-    },
-    {
-        "imageUrl": "images/image4.png",
-        "correct": false
-    }
+            "imageUrl": "images/image1.png",
+            "correct": false
+        },
+        {
+            "imageUrl": "images/image2.png",
+            "correct": true
+        },
+        {
+            "imageUrl": "images/image3.png",
+            "correct": false
+        },
+        {
+            "imageUrl": "images/image4.png",
+            "correct": false
+        }
     ]
 };
 
@@ -73,7 +73,7 @@ questions.push(question1);
 questions.push(question2);
 questions.push(question3);
 
-$(document).ready(function () {
+$(document).ready(function() {
     console.log("loaded");
     init();
 });
@@ -93,7 +93,7 @@ function init() {
     $("#qtype3-section").hide();
     $("#nextQuestion").hide();
 
-    $("#startQuiz").click(function () {
+    $("#startQuiz").click(function() {
         hideAllSections();
         username = $("#usernameinput").val();
         $("#name").html(username);
@@ -103,7 +103,7 @@ function init() {
         $("#nextQuestion").show();
     });
 
-    $("#nextQuestion").click(function () {
+    $("#nextQuestion").click(function() {
         if (currentQuestion < questions.length) {
             $("#nextQuestion").html("Next");
             loadQuestion(currentQuestion);
@@ -144,7 +144,7 @@ function appendTextExample() {
 }
 
 function loadQuestion(index) {
-    if (typeof (index) === 'undefined' || index === null) {
+    if (typeof(index) === 'undefined' || index === null) {
         index = 0;
     }
     var question = questions[index];
@@ -171,16 +171,16 @@ function loadQuestion(index) {
         qOneButton3.unbind("click");
         qOneButton4.unbind("click");
 
-        qOneButton1.click(function () {
+        qOneButton1.click(function() {
             checkAnswer(this);
         });
-        qOneButton2.click(function () {
+        qOneButton2.click(function() {
             checkAnswer(this);
         });
-        qOneButton3.click(function () {
+        qOneButton3.click(function() {
             checkAnswer(this);
         });
-        qOneButton4.click(function () {
+        qOneButton4.click(function() {
             checkAnswer(this);
         });
 
@@ -197,11 +197,11 @@ function loadQuestion(index) {
         img1.unbind("click");
         img2.unbind("click");
 
-        img1.click(function () {
+        img1.click(function() {
             console.log("img1 clicked");
         });
 
-        img2.click(function () {
+        img2.click(function() {
             console.log("img2 clicked");
         });
 
@@ -221,16 +221,16 @@ function loadQuestion(index) {
         $("#qtype3Img3").unbind("click");
         $("#qtype3Img4").unbind("click");
 
-        $("#qtype3Img1").click(function () {
+        $("#qtype3Img1").click(function() {
             checkAnswer(this);
         });
-        $("#qtype3Img2").click(function () {
+        $("#qtype3Img2").click(function() {
             checkAnswer(this);
         });
-        $("#qtype3Img3").click(function () {
+        $("#qtype3Img3").click(function() {
             checkAnswer(this);
         });
-        $("#qtype3Img4").click(function () {
+        $("#qtype3Img4").click(function() {
             checkAnswer(this);
         });
 
@@ -244,4 +244,96 @@ function checkAnswer(element) {
     $(element).attr("data-ans-val", true);
     console.log($(element).attr("data-ans-val"));
     answers.push($(element).attr("data-ans-val"));
+}
+
+function editor_load() {
+    var editor = $("#editor");
+    dragElement($(editor)[0]);
+    $("#editor_close").click(function() {
+        editor.hide();
+    });
+
+    $("#editor_save").click(function() {
+        addQuestion();
+    });
+
+    function addQuestion() {
+        var newQId = $("#newqId").val();
+        var newQType = $("select#newqType option:checked").val();
+        var newQText = $("#newqText").val();
+
+        // TODO: answers
+        var newQuestion = {
+            "id": newQId,
+            "type": newQType,
+            "questionText": newQText,
+            "answers": [{
+                    "imageUrl": "images/image1.png",
+                    "correct": false
+                },
+                {
+                    "imageUrl": "images/image2.png",
+                    "correct": true
+                },
+                {
+                    "imageUrl": "images/image3.png",
+                    "correct": false
+                },
+                {
+                    "imageUrl": "images/image4.png",
+                    "correct": false
+                }
+            ]
+        };
+        questions.push(newQuestion);
+
+        $("#newqId").val("");
+        $("select#newqType option:checked").prop("selected", false)
+        $("#newqText").val("");
+    }
+
+    editor.show();
+}
+
+function dragElement(elmnt) {
+    console.log(elmnt);
+    var pos1 = 0,
+        pos2 = 0,
+        pos3 = 0,
+        pos4 = 0;
+    if (document.getElementById(elmnt.id + "header")) {
+        /* if present, the header is where you move the DIV from:*/
+        document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+    } else {
+        /* otherwise, move the DIV from anywhere inside the DIV:*/
+        elmnt.onmousedown = dragMouseDown;
+    }
+
+    function dragMouseDown(e) {
+        e = e || window.event;
+        // get the mouse cursor position at startup:
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        // call a function whenever the cursor moves:
+        document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+        e = e || window.event;
+        // calculate the new cursor position:
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // set the element's new position:
+        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    }
+
+    function closeDragElement() {
+        /* stop moving when mouse button is released:*/
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
 }
